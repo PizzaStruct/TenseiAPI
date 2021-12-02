09e490c92fded55f2aa659367685ae2db5975018
yosa12978 <yosa12978@gmail.com>
2021-12-02 23:15:57 +0300

    manga handler update 1

--- internal/api/handlers/mangaHandler.go
+++ internal/api/handlers/mangaHandler.go
@@ -1,6 +1,22 @@
 package handlers
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/PizzaStruct/TenseiAPI/internal/pkg/models"
+	"github.com/PizzaStruct/TenseiAPI/internal/pkg/repos"
+	"github.com/gorilla/mux"
+)
+
 type IMangaHandler interface {
+	GetManga(w http.ResponseWriter, r *http.Request)
+	GetMangas(w http.ResponseWriter, r *http.Request)
+	SearchMangas(w http.ResponseWriter, r *http.Request)
+	InsertMangas(w http.ResponseWriter, r *http.Request)
+	RemoveManga(w http.ResponseWriter, r *http.Request)
 }
 
 type MangaHandler struct {
@@ -8,4 +24,71 @@ type MangaHandler struct {
 
 func NewMangaHandler() IMangaHandler {
 	return new(MangaHandler)
+}
+
+func (mh *MangaHandler) GetManga(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	mangaRepo := repos.NewMangaRepo()
+	manga, err := mangaRepo.GetManga(vars["id"])
+	if err != nil {
+		http.Error(w, "Manga not found", 404)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(manga)
+}
+
+func (mh *MangaHandler) GetMangas(w http.ResponseWriter, r *http.Request) {
+	page_str := r.URL.Query().Get("page")
+	page, err := strconv.Atoi(page_str)
+	if err != nil {
+		page = 1
+	}
+	mangaRepo := repos.NewMangaRepo()
+	mangas := mangaRepo.GetMangas(int64(page))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(mangas)
+}
+
+func (mh *MangaHandler) SearchMangas(w http.ResponseWriter, r *http.Request) {
+	page_str := r.URL.Query().Get("page")
+	page, err := strconv.Atoi(page_str)
+	if err != nil {
+		page = 1
+	}
+	query := r.URL.Query().Get("query")
+	mangaRepo := repos.NewMangaRepo()
+	mangas := mangaRepo.SearchManga(query, int64(page))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(mangas)
+}
+
+func (mh *MangaHandler) InsertMangas(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad request", 400)
+		return
+	}
+	var manga models.Manga
+	json.Unmarshal(body, &manga)
+	mangaRepo := repos.NewMangaRepo()
+	if err := mangaRepo.InsertManga(&manga); err != nil {
+		http.Error(w, "Bad request", 400)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write([]byte("{\"status\": 201, \"message\": \"Created\"}"))
+}
+
+func (mh *MangaHandler) RemoveManga(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	mangaRepo := repos.NewMangaRepo()
+	mangaRepo.RemoveManga(vars["id"])
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(202)
+	w.Write([]byte("{\"status\": 202, \"message\": \"Accepted\"}"))
 }
--- internal/api/routers/router.go
+++ internal/api/routers/router.go
@@ -3,9 +3,19 @@ package routers
 import (
 	"net/http"
 
+	"github.com/PizzaStruct/TenseiAPI/internal/api/handlers"
 	"github.com/gorilla/mux"
 )
 
 func InitRouter() http.Handler {
-	return mux.NewRouter()
+	router := mux.NewRouter().StrictSlash(true)
+	mangaHandler := handlers.NewMangaHandler()
+
+	router.HandleFunc("/mangas", mangaHandler.GetMangas).Methods("GET")
+	router.HandleFunc("/mangas/{id}", mangaHandler.GetManga).Methods("GET")
+	router.HandleFunc("/mangas/search", mangaHandler.SearchMangas).Methods("GET")
+	router.HandleFunc("/mangas", mangaHandler.InsertMangas).Methods("POST")
+	router.HandleFunc("/mangas/{id}", mangaHandler.RemoveManga).Methods("DELETE")
+
+	return router
 }
